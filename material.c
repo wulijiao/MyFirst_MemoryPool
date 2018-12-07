471a07de2279af7c337fcafb434be9cafe6c7a5a
wulijiao <1297831321@qq.com>
2018-12-07 21:54:52 +0800

    材料修改

---
+++ material.c
@@ -0,0 +1,5 @@
+the_way_of_java：      https://blog.csdn.net/qq_27026603/article/details/65946662
+digital_televation:      https://blog.csdn.net/datamining2005/article/details/53946059
+android-Java-Linux:      https://blog.csdn.net/luoshengyang/article/details/8923485
+EPG_introdution:       https://blog.csdn.net/datamining2005/article/details/54310996
+the_step_of_c/c++：   https://www.cnblogs.com/chengjian-physique/p/8076346.html
